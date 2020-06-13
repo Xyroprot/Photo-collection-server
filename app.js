8e70bc3966376d24b3ef97c23dbd8ad8eb17fd7d
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-13 17:57:14 +0300

    basic version

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 const express = require('express');
-const path = require('path');
-const cards = require('./routes/cards');
-const users = require('./routes/users');
 
 const { PORT } = process.env;
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', cards);
-app.use('/', users);
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- routes/cards.js
+++
@@ -1,9 +0,0 @@
-const cards = require('express').Router();
-const path = require('path');
-
-const filePath = path.resolve('data', 'cards.json');
-cards.get('/cards', (req, res) => {
-  res.sendFile(filePath);
-});
-
-module.exports = cards;
