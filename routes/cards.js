7a545203057846639892b12aed28689b1740837f
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-14 16:53:41 +0300

    Error-handlers added

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,25 @@
 const cards = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const fs = require('fs');
 const path = require('path');
 
 const filePath = path.resolve('data', 'cards.json');
-cards.get('/cards', (req, res) => {
-  res.sendFile(filePath);
+
+const sendCards = (async (req, res, next) => {
+  try {
+    const fileReader = await fs.createReadStream(filePath, { encoding: 'utf8' });
+    return fileReader.pipe(res);
+  } catch (error) {
+    // отправляет ошибку в обработчик
+    return next(error);
+  }
 });
+
+const cardsServerError = (asyncHandler(async () => {
+  throw new Error('Все сломалось... опять!');
+}));
+
+cards.get('/cards', sendCards);
+cards.get('/cards', cardsServerError);
 
 module.exports = cards;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
 const users = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
 const filePath = path.resolve('data', 'users.json');
+// const fileReader = fs.createReadStream(filePath, { encoding: 'utf8' });
 
-users.get('/users', (req, res) => {
-  res.sendFile(filePath);
+const sendUsers = (async (req, res, next) => {
+  try {
+    const fileReader = await fs.createReadStream(filePath, { encoding: 'utf8' });
+    return fileReader.pipe(res);
+  } catch (error) {
+    // отправляет ошибку в обработчик
+    return next(error);
+  }
 });
 
-users.get('/users/:id', (req, res) => {
+const usersServerError = (asyncHandler(async () => {
+  throw new Error('Все сломалось... опять!');
+}));
+
+const sendUserById = (async (req, res, next) => {
   fsPromises.readFile(filePath, { encoding: 'utf8' })
     .then((data) => {
       // eslint-disable-next-line no-underscore-dangle
@@ -18,9 +31,17 @@ users.get('/users/:id', (req, res) => {
       }
       res.send(JSON.parse(data)[index]);
     })
-    .catch((err) => {
-      throw err;
-    });
+    .catch(() => next(new Error('Все сломалось... опять!')));
 });
+
+users.get('/users', sendUsers);
+users.get('/users', usersServerError);
+users.get('/users/:id', sendUserById);
+
+/*
+users.get('/users', (req, res) => {
+  res.sendFile(filePath);
+});
+*/
 
 module.exports = users;
