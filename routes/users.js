fb8ca05ffb64ecda478bec4f1181c82fbd2f4070
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-17 12:55:26 +0300

    mongoose imported in project

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ const users = require('express').Router();
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
-const { json } = require('express');
 
 const filePath = path.resolve('data', 'users.json');
 
@@ -10,28 +9,31 @@ const sendUsers = (async (req, res) => {
   try {
     const fileReader = await fs.createReadStream(filePath, { encoding: 'utf8' });
     fileReader.pipe(res.type('json'));
-    return fileReader.on('error', (error) => {
+    fileReader.on('error', (error) => {
       console.log(error);
-      return res.status(500).send({ message: 'Произошла ошибка при чтении данных' });
+      res.status(500).send({ message: 'Произошла ошибка при чтении данных' });
     });
+    return;
   } catch (error) {
     console.log(error);
-    return res.status(500).send({ message: 'Произошла ошибка при чтении данных' });
+    res.status(500).send({ message: 'Произошла ошибка при чтении данных' });
   }
 });
 
-const sendUserById = (async (req, res, next) => {
+const sendUserById = (async (req, res) => {
   fsPromises.readFile(filePath, { encoding: 'utf8' })
     .then((data) => {
       const usersData = JSON.parse(data);
-      // eslint-disable-next-line no-underscore-dangle
       const userData = usersData.find((item) => item._id === req.params.id);
       if (userData) {
         return res.send(userData);
       }
       return res.status(404).send({ message: 'Нет пользователя с таким id' });
     })
-    .catch(() => next(new Error('Произошла ошибка при чтении данных')));
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send({ message: 'Произошла ошибка при чтении данных' });
+    });
 });
 
 users.get('/users', sendUsers);
