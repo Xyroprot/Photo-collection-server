1a79aaf873752ca2c8a232a0f0f13b3570da4177
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-17 17:42:05 +0300

    routes & controllers for users created

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 
 const { PORT } = process.env;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
---
+++ controllers/users.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при чтении данных' }));
+};
+
+const getUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      return res.send({ data: user });
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при чтении данных' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка при чтении данных' }));
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+};
