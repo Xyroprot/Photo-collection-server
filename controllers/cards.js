66df6df561c95dc87b1d220521291aa9c1cd1059
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-22 15:25:39 +0300

    middleware for authorization added

--- controllers/cards.js
+++ controllers/cards.js
@@ -33,7 +33,7 @@ const deleteCard = (req, res) => {
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если
+    { $addToSet: { likes: req.user._id } },
     {
       new: true,
       runValidators: true,
@@ -52,7 +52,7 @@ const likeCard = (req, res) => {
 const dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } },
     {
       new: true,
       runValidators: true,
---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const { token } = req.cookies.jwt;
+
+  if (!token) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'en!gma');
+  } catch {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+  req.user = payload;
+  return next();
+};
+
+module.exports = auth;
