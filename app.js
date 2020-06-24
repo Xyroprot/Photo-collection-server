f322a4f067dece97f94eeed8e8cd8f8775cc20a3
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-06-24 08:37:49 +0300

    request and error logging added

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const {
 
 const cards = require('./routes/cards');
 const users = require('./routes/users');
+
 const { login, createUser } = require('./controllers/users');
+
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { NotFoundError } = require('./errors/errors-bundle');
 
 const { PORT = 3000 } = process.env;
@@ -25,13 +29,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(requestLogger);
+
 app.post('/signin', celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
-
 app.post('/signup', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
@@ -42,13 +47,14 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use(errors());
-
 app.use('/', auth, cards);
 app.use('/', auth, users);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
+
+app.use(errorLogger);
+app.use(errors());
 app.use((err, req, res) => {
   const { statusCode = 500, message } = err; // если у ошибки нет кода, то по умолчанию код "500"
   res
